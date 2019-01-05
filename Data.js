235cf7158d3fbff792e90bb4b0d6a614eee63f3f
MobileWojciec <mobile.wojciech@gmail.com>
2019-01-05 15:35:08 +0200

    initial commit

---
+++ Data.js
@@ -0,0 +1,10 @@
+const data = [
+    { name: 'Tanya' },
+    { name: 'Elsa' },
+    { name: 'Anna' },
+    { name: 'Emma' },
+    { name: 'Jack' },
+    { name: 'Neo' },
+    { name: 'Lo' },
+    { name: 'Jane' },
+]
